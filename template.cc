c7af3d70175b1e21ebbb3e1076c49318640ccd4a
punkieL <y422891293@gmail.com>
2016-02-24 04:25:43 +0900

    add template.cc

---
+++ template.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main(int args, char *argc[]){
+
+  return 0;
+}
+
