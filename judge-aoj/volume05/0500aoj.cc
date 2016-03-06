35c0a9d544cc8c2ebf7d2a9cba39b5ea77cddaf5
punkieL <united.ichi@gmail.com>
2016-03-07 03:40:53 +0900

    add 0500aoj.cc I need to learn more about github.....orz

---
+++ judge-aoj/volume05/0500aoj.cc
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main(int args, char *argc[]){
+  int n;
+  while(cin>>n){
+    if(n==0)
+      return 0;
+    int a=0,b=0;
+    for(int i=0;i<n;i++){
+      int ia,ib;
+      cin>>ia>>ib;
+      if(ia==ib){
+        a+=ia;
+        b+=ib;
+      }
+      else if(ia<ib)
+        b+=(ia+ib);
+      else
+        a+=(ia+ib);
+    }
+    printf("%d %d\n",a,b);
+  }
+  return 0;
+}
+
