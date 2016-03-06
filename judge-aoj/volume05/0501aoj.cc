7bde3c523ab2b4ec0e6a1e5205f5e182a935b7dd
punkieL <united.ichi@gmail.com>
2016-03-07 04:05:10 +0900

    add 0501aoj.cc

---
+++ judge-aoj/volume05/0501aoj.cc
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main(int args, char *argc[]){
+  int n;
+  while(cin>>n){
+    if(n==0) return 0;
+    map<char,char> table;
+    for(int i=0;i<n;i++){
+      char a,b;
+      cin>>a>>b;
+      table.insert(make_pair(a,b));
+    }
+
+    cin>>n;
+    for(int i=0;i<n;i++){
+      char a;
+      cin>>a;
+      if(table.count(a)!=0)
+        cout<<table[a];
+      else
+        cout<<a;
+    }
+    cout<<endl;
+    }
+  return 0;
+}
---
+++ judge-aoj/volume05/readme-volumn05.md
@@ -0,0 +1,2 @@
+#####volumn 05
+problems in volumn 05 should be the basic problems.
