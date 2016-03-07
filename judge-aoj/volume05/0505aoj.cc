4f85215aaa935a57797ffa4b81506b39afbb4d85
candywater <y422891293@gmail.com>
2016-03-08 04:26:01 +0900

    add 0505aoj.cc
    one should be clam down when he is programming....
    otherwise he won't find the bug forever...maybe...

---
+++ judge-aoj/volume05/0505aoj.cc
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main(int args, char *argc[]){
+  int student,locations;
+  //ifstream ifs("input.txt");
+  while(cin>>student>>locations, student&&locations){
+    int wishes[locations];
+
+    for(int i=0;i<locations;i++)
+      wishes[i]=0;//,printf("wishes[%d]:%d ",i,wishes[i]);printf("\n");
+    int stu=student;
+    while(stu--){
+      int loc=locations;
+      while(loc--){
+        int tmp;cin>>tmp;
+        wishes[locations-loc-1]+=tmp;
+      }
+    }
+
+    /*for(int i=0;i<locations;i++)
+      printf("wishes[%d]:%d ",i,wishes[i]);printf("\n");*/
+
+    map<double,int> res;
+    int loc=locations;
+    while(loc--){
+      res.insert(make_pair((double)wishes[loc]-(double)loc/100,loc));
+    }
+
+    for(map<double,int>::iterator iz=(--res.end());
+    iz!=res.begin();iz--)
+      //cout<<"("<<(iz->first)<<")",
+      cout<<(iz->second)+1<<" ";
+    cout<<(res.begin())->second+1<<"\n";
+  }
+  return 0;
+}
