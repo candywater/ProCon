f220725c7d183ba3d04ec92f21efca9d086710ed
candywater <candywater@users.noreply.github.com>
2018-02-07 23:21:34 +0900

    cannot pass the last case even use set to aviod 'aa...a'

---
+++ leetcode/5-longest-palindromic-substring/5.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+    }
+
+    string reverseStr(string s){
+      string res(s.size());
+      for(int i = 0; i < s.size(); i++){
+        res
+      }
+    }
+};
