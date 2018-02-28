5a394a4146b3562eaf9b43948b898c2bbfd69536
candywater <candywater@users.noreply.github.com>
2018-02-28 23:51:54 +0900

    easy questions

---
+++ judge_online/AtCoder/abc087/a.cc
@@ -0,0 +1,13 @@
+#include "iostream"
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  int x, a, b;
+  cin >> x >> a >> b;
+  cout << (x-a)%b << endl;
+  /* code */
+  //cout << "aa" <<endl;
+  return 0;
+}
