3470b769c6d65e7d83725908670ca79be68b5506
candywater <candywater@users.noreply.github.com>
2018-03-22 01:49:45 +0900

    change getdeepth to a general purpose function

--- leetcode/101-200/104-maximum-depth-of-binary-tree/104.cc
+++ leetcode/101-200/104-maximum-depth-of-binary-tree/104.cc
@@ -7,20 +7,21 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-public:
-  int maxDepth(TreeNode* root) {
-    return dfs(root);
-  }
+ class Solution {
+ public:
+   int maxDepth(TreeNode* root) {
+     return dfs(root);
+   }
 
-  const int& max(const int& a, const int& b){
-    return a < b ? b : a;
-  }
+   const int& max(const int& a, const int& b){
+     return a < b ? b : a;
+   }
 
-  int dfs(TreeNode* p){
-    if(p == NULL) return 0;
-    else {
-      return max(dfs(p->right) + 1, dfs(p->left) + 1);
-    }
-  }
-};
+   template<typename T>
+   int dfs(T* p){
+     if(p == NULL) return 0;
+     else {
+       return max(dfs(p->right) + 1, dfs(p->left) + 1);
+     }
+   }
+ };
