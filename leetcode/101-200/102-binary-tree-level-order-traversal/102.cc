c509498c4424daa862745df5c0eb31dab0530dcc
candywater <candywater@users.noreply.github.com>
2018-03-22 02:13:19 +0900

    add solution 102, 103, 637

---
+++ leetcode/101-200/102-binary-tree-level-order-traversal/102.cc
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ */
+
+#include "iostream"
+#include "vector"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+  vector<vector<int>> levelOrder(TreeNode* root) {
+   int deepth = getdeepth(root);
+   vector<vector<int>> res(deepth, vector<int>());
+   dfs(root, res, 0);
+   return res;
+  }
+
+  template<typename T>
+  void dfs(T* head, vector<vector<int>>& res, int deepth){
+   if(head == NULL) return;
+   res[deepth].push_back(head->val);
+
+   dfs(head->left, res, deepth + 1);
+   dfs(head->right, res, deepth + 1);
+  }
+
+  template<typename T>
+  int getdeepth(T* head){
+   if(head == NULL) return 0;
+   else return max(getdeepth(head->right), getdeepth(head->left)) + 1;
+  }
+};
+
+ int main(int argc, char const *argv[]) {
+   /* code */
+   return 0;
+ }
