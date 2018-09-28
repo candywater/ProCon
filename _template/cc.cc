f6c1a27c1bbb7b0af79c8421f68f0694d995a1ef
candywater <candywater@users.noreply.github.com>
2018-09-28 21:29:26 +0800

    fix template

--- _template/cc.cc
+++ _template/cc.cc
@@ -4,12 +4,18 @@ using namespace std;
 
 
 template<typename T>
+void show(T a);
+
+
+int main(int argc, char *argv[]){
+}
+
+
+
+template<typename T>
 void show(T a){
   for(auto i : a){
 
   }
   cout << endl;
-}
-
-int main(int argc, char *argv[]){
 }
