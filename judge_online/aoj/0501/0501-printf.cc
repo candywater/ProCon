d6035d64b5144dd6f4a99770e9cb264b1f52521f
candywater <candywater@users.noreply.github.com>
2018-09-29 10:54:26 +0800

    for some reason rewrite 0501.... = ="||

--- judge_online/aoj/0501/0501-printf.cc
+++ judge_online/aoj/0501/0501-printf.cc
@@ -1,10 +1,7 @@
-// rewrite it in 2018
-
 #include <iostream>
 #include <unordered_map>
 
 using namespace std;
-
 
 template<typename T> void show(T a){
   for(auto i : a){}
@@ -14,23 +11,23 @@ template<typename T> void show(T a){
 int main(int argc, char *argv[]){
   int n, m;
   char t1, t2;
-  cin >> n;
+  scanf("%d\n", &n);
   while(n != 0){
     unordered_map<char, char> nummap;
     for(int i = 0; i < n; i++){
-      cin >> t1 >> t2;
+      scanf("%c %c\n", &t1, &t2);
       nummap.insert({t1, t2});
     }
-    cin >> m;
+    scanf("%d\n", &m);
     for(int i = 0; i < m ; i++){
-      cin >> t1;
+      scanf("%c\n", &t1);
       if(nummap.count(t1) != 0)
-        cout << nummap[t1];
+        printf("%c", nummap[t1]);
       else
-        cout << t1;
+        printf("%c", t1);
     }
-    cout << endl;
-    cin >> n;
+    printf("\n");
+    scanf("%d\n", &n);
   }
   return 0;
 }
