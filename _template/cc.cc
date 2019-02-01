e4c70f34540b80a56003481709101a891b5179a1
candywater <candywater@users.noreply.github.com>
2019-02-01 21:24:56 +0800

    changed template g++

--- _template/cc.cc
+++ _template/cc.cc
@@ -2,12 +2,18 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 using namespace std;
 
 template<typename T> 
 void show(T a){
   for(auto i : a){ cout << i << " "; }
+  cout << endl;
+}
+template<typename T> 
+void showmatrix(T a){
+  for(auto j: a){for(auto i : j){ cout << i; }cout << endl;}
   cout << endl;
 }
 
