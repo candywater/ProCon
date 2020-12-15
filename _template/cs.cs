4959efb6b5a8eb279a7543c93b8dd68c0ea4c912
candywater <candywater@github.com>
2020-12-15 09:58:27 +0800

    update cs.cs

--- _template/cs.cs
+++ _template/cs.cs
@@ -2,7 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-
-static void Main()
+public class Solution
 {
+  readonly int[] dx = new int[]{0, 1, 0, -1};
+  readonly int[] dy = new int[]{1, 0, -1, 0};
+
+// ↓-- debug --↓
+  static void Show<T>(T list){
+    foreach(var i in list){
+      Console.Write($@"{i} ");
+    }
+    Console.WriteLine();
+  }
+  static void ShowMatrix<T>(T matrix){
+    foreach(var i in matrix){
+      foreach(var j in matrix){
+        Console.Write($"{i} ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+// ↑-- debug --↑
+
+  static void Main()
+  {
+  }
 }
