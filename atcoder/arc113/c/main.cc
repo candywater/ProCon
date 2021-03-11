8082516ce4b479d415820be47395689977884073
candywater <candywater@users.noreply.github.com>
2021-03-12 00:45:09 +0800

    実装がむずい......

--- atcoder/arc113/c/main.cc
+++ atcoder/arc113/c/main.cc
@@ -10,7 +10,7 @@ using namespace std;
 template <typename T>
 void show(T a) {
   for (auto i : a) {
-    cout << i << " ";
+    cout << i.first << " " << i.second << " ";
   }
   cout << endl;
 }
@@ -31,6 +31,14 @@ void check(char i = ' ', int j = 0) {
   cout << "checkpoint:[" << i << "," << j << "]" << endl;
 }
 
+unordered_map<char, long long> map;
+
+void setMapZero() {
+  for (auto &i : map) {
+    i.second = 0;
+  }
+}
+
 int main(int argc, char *argv[]) {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
@@ -38,82 +46,27 @@ int main(int argc, char *argv[]) {
   string str;
   cin >> str;
 
-  if (str.size() <= 0) return 0;
+  if (str.size() < 3) return 0;
+
+  for (int i = 0; i < 26; i++) {
+    map.insert({i + 'a', 0});
+  }
 
   // https://atcoder.jp/contests/arc113/editorial/710
-  // block: aaaa in aaaasss
-  // block: bbb in abdbbbd
+  // https://atcoder.jp/contests/arc113/submissions/20811170
 
-  vector<int> pos;
-  vector<char> block;
-  vector<int> blocklen;
+  long long res = 0;
 
-  // collect blocks
-  char tmp = str[0];
-  for (int i = 1; i < str.size(); i++) {
-    if (tmp == str[i]) {
-      pos.push_back(i - 1);
-      block.push_back(str[i]);
-      int len = 2;
-      while (tmp == str[++i]) {
-        len++;
-      }
-      blocklen.push_back(len);
-    }
-    if (tmp != str[i]) {
-      tmp = str[i];
+  map[str.back()] += 1;
+  for (long long i = str.size() - 2; i >= 0; i--) {
+    map[str[i]] += 1;
+    if (str[i] == str[i + 1]) {
+      long long righthand = str.size() - i;
+      res += righthand - map[str[i]];
+      setMapZero();
+      map[str[i]] = righthand;
     }
   }
-
-  show(pos);
-  show(block);
-  show(blocklen);
-
-  int res = 0;
-
-  int j = 0;
-  tmp = block[j];
-  for (int i = pos[j] + blocklen[j]; i < str.size(); i = pos[j] + blocklen[j]) {
-    while (i < str.size() && tmp != str[i++]) {
-      res++;
-    }
-    j++;
-    while (j < block.size() && (block[j] == block[j - 1] || pos[j] < i)) {
-      j++;
-    }
-    tmp = block[j];
-  }
-
-  // // analyze blocks
-  // // if same blocks, merge them
-  // for (int i = 0; i + 1 < pos.size(); i++) {
-  //   if (block[i] == block[i + 1]) {
-  //     blocklen[i] += blocklen[i + 1];
-  //     pos[i + 1] = -1;
-  //     block[i + 1] = '\0';
-  //     blocklen[i + 1] = -1;
-  //   }
-  // }
-
-  // // erase them
-  // for (int i = pos.size() - 1; i >= 0; i--) {
-  //   if (pos[i] == -1) {
-  //     pos.erase(pos.begin() + i);
-  //     block.erase(block.begin() + i);
-  //     blocklen.erase(blocklen.begin() + i);
-  //   }
-  // }
-
-  // long res = 0;
-  // for (int i = 0; i + 1 < pos.size(); i++) {
-  //   int left = pos[i];
-  //   int right = pos[i + 1];
-  //   res += (right - left) - blocklen[i];
-  // }
-  // int left = pos[pos.size() - 1];
-  // int right = str.size();
-  // cout << "left:" << left << " right:" << right << endl;
-  // res += (right - left) - blocklen[blocklen.size() - 1];
 
   cout << res << endl;
 }
