11027cd01f9028454a93cd4768c7a9f219a90053
candywater <candywater@users.noreply.github.com>
2021-03-20 21:32:13 +0800

    delete comment

--- atcoder/abc196/c/c.cc
+++ atcoder/abc196/c/c.cc
@@ -55,14 +55,6 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  auto isdoubled = [](string& s) -> bool {
-    ll len = s.size() / 2;
-    rep(i, len) {
-      if (s[i] != s[i + len]) return false;
-    }
-    return true;
-  };
-
   string num = to_string(n);
   if (num.size() % 2 == 0) {
     res = 0;
@@ -79,46 +71,6 @@ int main(int argc, char* argv[]) {
   4:90
   6:900
   */
-  // ll m = n;
-  // ll res = 0;
-
-  // m = n;
-  // ll digitcount = 0;
-  // while (m > 0) {
-  //   digitcount++;
-  //   m /= 10;
-  // }
-
-  // if (digitcount % 2 == 0) {
-  //   string num = to_string(n);
-  //   ll tmp = num[num.size() / 2 - 1] - '0';
-  //   ll zerocount = 10;
-  //   rep3(i, num.size() / 2 - 2, 0) {
-  //     if (i == 0) {
-  //       tmp += (num[i] - '0') * zerocount;
-  //     } else {
-  //       tmp += (num[i] - '0' + 1) * zerocount;
-  //     }
-  //     zerocount *= 10;
-  //   }
-  //   string before = num.substr(0, num.size() / 2);
-  //   string after = num.substr(num.size() / 2, num.size());
-  //   if (after < before) {
-  //     res += tmp - 1;
-  //   } else {
-  //     res += tmp;
-  //   }
-  //   // cout << tmp << endl;
-  // } else {
-  //   int count = 9;
-  //   m = n;
-  //   m = m / 100;
-  //   res = 0;
-  //   while (m > 0) {
-  //     res += count;
-  //     m = m / 100;
-  //     count *= 10;
-  //   }
-  // }
+  // 
   cout << res << endl;
 }
